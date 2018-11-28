a2bb1f157e2b0d750cf8391938ffc6f37b257fdb
Wei Huiping <whplcyz@gmail.com>
2018-11-28 21:19:04 +0800

    Init code for security server

---
+++ src/main/java/com/whp/platform/sccommonplatform/ScCommonPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.whp.platform.sccommonplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScCommonPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScCommonPlatformApplication.class, args);
+	}
+}
---
+++ src/main/java/com/whp/platform/sccommonplatform/web/Login.java
@@ -0,0 +1,8 @@
+package com.whp.platform.sccommonplatform.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Login {
+
+}
